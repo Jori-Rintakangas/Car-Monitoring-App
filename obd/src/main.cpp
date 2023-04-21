52b98b65b45957de7ceadb7dce65b36aaaf3567c
Jori Rintakangas <jori.rintakangas@tuni.fi>
2023-04-21 13:30:25 +0300

    Folder for ESP32 codes

---
+++ obd/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <WiFi.h>
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+
+}
