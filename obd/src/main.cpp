bc6e2c43dea9b854cb4d3b3964e3955634c92fee
Jori Rintakangas <jori.rintakangas@tuni.fi>
2023-04-26 12:08:30 +0300

    Added k line library

--- obd/src/main.cpp
+++ obd/src/main.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-#include <WiFi.h>
 #include "BluetoothSerial.h"
+#include "OBD9141.h"
+
+#define VEHICLE_SPEED 0x0D
+#define ENGINE_RPM 0x0C
+#define TEMPERATURE 0x05
+#define MASS_AIR_FLOW 0x10
+#define ENGINE_LOAD 0x04
+#define THROTTLE_POS 0x11
 
 BluetoothSerial SerialBT;
 
 void setup()
 {
-    
+    SerialBT.begin("OBD");
 }
 
 void loop()
